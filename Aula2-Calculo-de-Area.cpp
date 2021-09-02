0ce38e74f0e7bf675826234b409ae2ceaefe2160
allrox <alexandre@allrox.com.br>
2021-09-02 10:38:26 -0300

    Inclusão de exemplo para cálculo de área e mudanças de nomes de arquivos

---
+++ Aula2-Calculo-de-Area.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+float calculoDeArea (float, float, float);
+
+int main(){
+	
+	setlocale(LC_ALL,"Portuguese");
+	int l,c,a;
+	
+	cout<<"\nInforme a largura do ambiente em metros:";
+	cin>>l;
+	cout<<"Informe o comprimento do ambiente em metros:";
+	cin>>c;
+	cout<<"Informe a altura do ambiente em metros:";
+	cin>>a;
+	cout<<"\nA área total é igual a "<<calculoDeArea(l,c,a)<<"m³";
+	cout<<"\n\n";
+	
+	system("pause");
+	return 0;
+	
+}
+
+float calculoDeArea(float l, float c, float a)
+{
+	return l*c*a;
+}
